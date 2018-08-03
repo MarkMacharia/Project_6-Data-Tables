dc7e27410f01a9d630ed8569cabddb12725a101c
MarkMacharia <marcmacharia@gmail.com>
2018-08-03 16:20:48 +0300

    did basic and advanced calculator

---
+++ Project 4/basic_calculator.js
@@ -0,0 +1,4 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "8";//dispaly 8
+    document.getElementById("display").innerHTML = "3 + 5 = 8";//display 3 + 5=8
+}
---
+++ Project 5/advanced_calculator.js
@@ -0,0 +1,12 @@
+function myFunction() {
+ //   document.getElementById("one").value=
+   //   document.getElementById("two").value;
+      
+//document.getElementById("answer").value = Number( document.getElementById("two").value) + Number(document.getElementById("one").value); 
+  
+  var x = Number( document.getElementById("one").value);//picking x value
+  var y = Number( document.getElementById("two").value);//picking y value
+      
+   document.getElementById("answer").value = x + y; //Adding value x and y
+
+} 
